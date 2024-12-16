8a2a81620c1547d1dcd409d7a018778b835b6f1f
Abdulloh <Abdullohtest1@gmail.com>
2024-12-16 14:42:36 +0300

    3-qator

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1-qator");
         System.out.println("2-qator");
+        System.out.println("3-qator");
     }
 }
